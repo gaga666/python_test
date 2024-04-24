9488c45f87583c26c826e4bcffe8be1bfd32a079
gamemory <guagua.id@gmail.com>
2024-04-24 16:54:02 +0800

    上传测试

---
+++ iris/test.py
@@ -0,0 +1,13 @@
+from sklearn import datasets
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+
+iris_data = pd.read_csv(r'Iris.csv',header=None)
+
+
+
+
+
